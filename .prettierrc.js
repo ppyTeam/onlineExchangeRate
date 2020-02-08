c1506cf9de57ce9d2a88eb36780ab94e78f90826
ttionya <git@ttionya.com>
2020-02-08 21:36:03 +0800

    style 新增代码格式化配置

---
+++ .prettierrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  // 一行最多 120 字符
+  printWidth: 120,
+  // 使用 2 个空格缩进
+  tabWidth: 2,
+  // 使用空格替代缩进符
+  useTabs: false,
+  // 行尾不使用分号
+  semi: false,
+  // 使用单引号
+  singleQuote: true,
+  // 对象的 key 仅在必要时用引号
+  quoteProps: 'as-needed',
+  // jsx 使用双引号
+  jsxSingleQuote: false,
+  // 添加 es5 支持的尾逗号
+  trailingComma: 'es5',
+  // 大括号内的首尾需要空格
+  bracketSpacing: true,
+  // jsx 标签的反尖括号需要换行
+  jsxBracketSameLine: false,
+  // 箭头函数需要括号
+  arrowParens: 'always',
+  // 每个文件格式化的范围是文件的全部内容
+  rangeStart: 0,
+  rangeEnd: Infinity,
+  // 不需要写文件开头的 @prettier
+  requirePragma: false,
+  // 不需要自动在文件开头插入 @prettier
+  insertPragma: false,
+  // 使用默认的折行标准
+  proseWrap: 'preserve',
+  // 根据显示样式决定 html 要不要折行
+  htmlWhitespaceSensitivity: 'css',
+  // 格式化 vue 文件的 script 和 style
+  vueIndentScriptAndStyle: true,
+  // 换行符使用 lf
+  endOfLine: 'lf',
+}
